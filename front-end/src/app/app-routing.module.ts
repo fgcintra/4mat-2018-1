dad06bcde986a6cf9f851dc670a0c39c434deabf
fgcintra <fausto@fgcintra.pro.br>
2018-05-07 15:56:19 -0300

    (07/05) Criação do serviço e do componente de listagem da categoria

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,13 +1,20 @@
 import { ProdutoService } from './produto/produto.service';
+import { CategoriaService } from './categoria/categoria.service';
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { ProdutoListComponent } from './produto/produto-list/produto-list.component';
+import { CategoriaListComponent } from './categoria/categoria-list/categoria-list.component';
 
-const routes : Routes = [
+const routes: Routes = [
   {
     path: 'produto',
     component: ProdutoListComponent
+  },
+  {
+    path: 'categoria',
+    component: CategoriaListComponent
   }
 ];
 
--- front-end/src/app/categoria/categoria-list/categoria-list.component.ts
+++ front-end/src/app/categoria/categoria-list/categoria-list.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { CategoriaService } from '../categoria.service';
 
 @Component({
   selector: 'app-categoria-list',
   templateUrl: './categoria-list.component.html',
-  styleUrls: ['./categoria-list.component.css']
+  styleUrls: ['./categoria-list.component.css'],
+  providers: [CategoriaService]
 })
 export class CategoriaListComponent implements OnInit {
 
-  constructor() { }
+  public categorias: any = {};
+
+  constructor(private cs: CategoriaService) { }
 
   ngOnInit() {
+    this.cs.listar().subscribe(
+      dados => this.categorias = dados,
+      erro => console.error(erro)
+    );
   }
 
 }
